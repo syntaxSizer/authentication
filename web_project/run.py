98a69302eac59ae010eb6ca88fe5906170749988
syntaxSizer <kakuma4never@gmail.com>
2016-05-13 13:32:13 +0300

    rearrange file dirctories

--- web_project/run.py
+++ web_project/run.py
@@ -1,13 +1,15 @@
 #!flask/bin/python
 
-
-
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return '<h1> Authentication initialize... !</h1>'
 
+
+@app.route('/')
+def index():
+    return '<h1>Authentication processing...!</h1>'
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
 if __name__ == '__main__':
-    app.run()
+   app.run(debug=True)
