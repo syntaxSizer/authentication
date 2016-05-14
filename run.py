e1034a3ec7b4fc3673ac4e9703f18677c3f2f8be
syntaxSizer <kakuma4never@gmail.com>
2016-05-14 10:58:40 +0300

    install flask-script extansion

--- run.py
+++ run.py
@@ -1,15 +1,21 @@
 #!web_project/flask/bin/python
 
 from flask import Flask
+from flask.ext.script import Manager
+
 app = Flask(__name__)
+
+manager = Manager(app)
 
 
 
 @app.route('/')
 def index():
     return '<h1>Authentication processing...!</h1>'
+
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, %s!</h1>' % name
+
 if __name__ == '__main__':
-   app.run(debug=True)
+   manager.run()
--- web_project/app/views.py
+++
@@ -1,5 +0,0 @@
-from app import app
-
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, %s!</h1>' % name
