c832f9319a44cab9a18a73e516337e8a586772d8
syntaxSizer <kakuma4never@gmail.com>
2016-05-15 13:22:03 +0300

    add form

--- run.py
+++ run.py
@@ -1,16 +1,30 @@
 #!web_project/flask/bin/python
 from flask import Flask,render_template
 #from flask.ext.script import Manager
-#from flask.ext.bootstrap import Bootstrap
-# ...
-#bootstrap = Bootstrap(app)
-
+#from flask.ext.moment import Moment
+#from datetime import datetime
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Required
+#moment = Moment(app)
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 #manager = Manager(app)
 
 
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    password=PasswordField('PassWord',validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+
+"""
+@app.route('/')
+def index():
+    return render_template('index.html', current_time=datetime.utcnow())
+"""
 
 @app.route('/')
 def index():
@@ -26,9 +40,6 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
-
-
-
 
 
 
