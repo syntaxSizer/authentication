ba0557394072fcb98e1510c835a394793ae93fb1
syntaxSizer <kakuma4never@gmail.com>
2016-05-16 11:34:06 +0300

    add bootstrap extention

--- run.py
+++ run.py
@@ -1,50 +1,23 @@
 #!web_project/flask/bin/python
-from flask import Flask,render_template
+from flask import Flask, render_template
 #from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
-from datetime import datetime
 from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import Required
-
-
-moment = Moment(app)
-Bootstrap = Bootstarp(app)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 #manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
-
-  
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    name = None
-    form = NameForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
-
-
-
-
-
-
-
-@app.route('/')
-def index():
-    return render_template('index.html', current_time=datetime.utcnow())
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
 
 
 @app.errorhandler(404)
@@ -57,7 +30,16 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
+
 
 if __name__ == '__main__':
- #  manager.run()
-    app.run(debug=True)
+   # manager.run()
+   app.run(debug=True)
