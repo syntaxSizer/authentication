a0243b3bddcf021d9de662457444af711ea8a7d9
syntaxSizer <kakuma4never@gmail.com>
2016-05-16 10:58:13 +0300

    add bootstarp extention

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 #!web_project/flask/bin/python
 from flask import Flask,render_template
 #from flask.ext.script import Manager
-#from flask.ext.moment import Moment
-#from datetime import datetime
+from flask.ext.moment import Moment
+from datetime import datetime
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Required
-#moment = Moment(app)
+
+
+moment = Moment(app)
+Bootstrap = Bootstarp(app)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -34,23 +37,21 @@ def index():
 
 
 
-"""
-@app.route('/')
-def index():
-    return render_template('index.html', current_time=datetime.utcnow())
-"""
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
